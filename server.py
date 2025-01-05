0d6406b75d1a0f98ea09880bda1c1dd730e3126a
dhudetz <dannyhudetz@gmail.com>
2025-01-04 18:17:16 -0600

    Initial server commit

---
+++ server.py
@@ -0,0 +1,18 @@
+import websockets
+import asyncio
+
+CONNECTIONS = set()
+
+async def echo(websocket):
+    if websocket not in CONNECTIONS:
+        CONNECTIONS.add(websocket)
+    async for message in websocket:
+        websockets.broadcast(CONNECTIONS,message)
+
+async def main():
+    async with websockets.serve(echo, "localhost", 8765):
+        await asyncio.Future()
+
+
+
+asyncio.run(main())
