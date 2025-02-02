805688b4537f711b942bd94bfdd82f8aa71106ab
dhudetz <dannyhudetz@gmail.com>
2025-02-01 21:25:43 -0600

    .gitignore

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,92 +1,84 @@
 import { useState, useEffect, useRef } from 'react';
-import GameScreen from './GameScreen';
+import Login from './features/Login/Login';
+import Lobby from './features/Lobby/Lobby';
+import Game from './features/Game/Game';
 import './App.css';
 
 function App() {
     console.log("app rerendered.")
     const [serverMessage, setServerMessage] = useState("")
-    const [username, setUsername] = useState("");
     const [errorMessage, setErrorMessage] = useState("");
+    const [currentScreen, setCurrentScreen] = useState("login");
     const webSocketRef = useRef(null); 
 
     useEffect(() => {
+        setTimeout(() => {createConnection();}, 1000);
+        return () => {
+            if (webSocketRef.current) {
+                webSocketRef.current.close();
+                webSocketRef.current.removeEventListener("open", handleOpen);
+                webSocketRef.current.removeEventListener("message", handleMessage);
+                webSocketRef.current.removeEventListener("close", handleClose);
+            }
+        };
+    }, []);
+
+    function createConnection(){
         const currentUrl = window.location.hostname;
         const webSocketUrl = `ws://${currentUrl}:8765`;
-
-        toggleScreens()
         
         webSocketRef.current = new WebSocket(webSocketUrl);
 
+        webSocketRef.current.addEventListener("open", (event) => {console.log("Connected!")});
+
         webSocketRef.current.addEventListener("message", (event) => {
             let messageData = JSON.parse(event.data);
-            setServerMessage(messageData)
+            setServerMessage(messageData);
             
             if(messageData.messageType === "error"){
-                console.log(messageData.payload)
-                setErrorMessage(messageData.payload)
+                console.log(messageData.payload);
+                setErrorMessage(messageData.payload);
                 setUsername("");
             }
             else if(messageData.messageType === "usernameCreated" || messageData.messageType === "usernameChange") {
                 console.log("Username accepted/changed!");
                 setErrorMessage("");
-                toggleScreens();
+                showScreen("lobby");
             }
         });
 
-        return () => {
-            if (webSocketRef.current) {
-                webSocketRef.current.close();
-            }
-        };
-    }, []);
+        webSocketRef.current.addEventListener("close", (event) => {
+            console.log('Websocket failed:', event);
+            setTimeout(() => {createConnection();}, 1000);
+        });
 
-    function toggleScreens(){
-        let loginScreenElement = document.getElementById("login-screen")
-        let gameScreenElement = document.getElementById("game-screen")
-        if (gameScreenElement.style.display === "none") {
-            gameScreenElement.style.display = "block";
-            loginScreenElement.style.display = "none";
-        } else {
-            gameScreenElement.style.display = "none";
-            loginScreenElement.style.display = "block";
+    }
+
+    function showScreen(screenID) {
+        setCurrentScreen(screenID);
+    }
+
+    function sendMessage(messageType, payload) {
+        if (!webSocketRef.current || webSocketRef.current.readyState !== WebSocket.OPEN) {
+            console.error("WebSocket is not connected.");
+            return;
         }
+    
+        const outgoingMessage = JSON.stringify({ messageType, payload });
+        webSocketRef.current.send(outgoingMessage);
     }
-
-    function sendMessage(messageType, payload){
-        if (webSocketRef.current && webSocketRef.current.readyState === WebSocket.OPEN) {
-            let outgoingMessage = JSON.stringify({ messageType: messageType, payload: payload });
-            webSocketRef.current.send(outgoingMessage);
-        } else {
-            console.error("WebSocket is not open. Unable to send message.");
-        }
-    }
-
-    function sendUsername(){
-        sendMessage("username", username)
-    }
-
-    const handleUsernameChange = (event) => setUsername(event.target.value);
 
     return (
         <>
-            <div id="login-screen">
-                <h1 className="title">cum brains spoons</h1>
-                <form onSubmit={(e) => e.preventDefault()}>
-                    <h2>choose your username:</h2>
-                    <input 
-                        type="text"
-                        value={username}
-                        onChange={handleUsernameChange} 
-                        onKeyDown={(e) => e.key === "Enter" && sendUsername()} 
-                    />
-                </form>
-                <p className='error'>{errorMessage}</p>
-                <button onClick={sendUsername}>Enter Game</button>
-            </div>
-            <div id="game-screen">
-                <GameScreen serverMessage = {serverMessage} sendMessage = {sendMessage} errorMessage={errorMessage}/>
-            </div>
-
+            {currentScreen === "login" && (
+                <Login serverMessage={serverMessage} sendMessage={sendMessage} errorMessage={errorMessage} />
+            )}
+            {currentScreen === "lobby" && (
+                <Lobby serverMessage={serverMessage} sendMessage={sendMessage} errorMessage={errorMessage} />
+            )}
+            {currentScreen === "game" && (
+                <Game serverMessage={serverMessage} sendMessage={sendMessage} errorMessage={errorMessage} />
+            )}
         </>
     );
 }
